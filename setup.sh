9b506a0ec4bb225b25cba320844d51c79552c8e5
Dan Corrigan <df.corrigan@gmail.com>
2020-11-23 11:24:27 -0500

    set up gitignore

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 rm -f ~/.zshrc && ln -s ~/dotfiles/zshrc ~/.zshrc
 rm -f ~/.gitconfig && ln -s ~/dotfiles/gitconfig ~/.gitconfig
+rm -f ~/.gitignore && ln -s ~/dotfiles/gitignore ~/.gitignore
 rm -f ~/.tmux.conf && ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
